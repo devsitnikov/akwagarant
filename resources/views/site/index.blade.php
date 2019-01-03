68bbd2a4c0a46236b8345abe6764e667c38eeec4
Sitnikov Roman <dev.sitnikov@gmail.com>
2019-01-03 17:08:42 +0300

    make_landing_header

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.site')
+
+@section('content')
+ <header class="main-header landing-header" style="background-image: url('{{asset('img/headbg.jpg')}}')">
+   <div class="content">
+       <h1 class="header-title">Монтаж систем отопления в частном доме в Москве
+           и области "под ключ" с гарантией до 7 лет
+           и по ценам на 10% ниже рыночных</h1>
+       <h2 class="header-description">Пройдите тест за 20 секунд и получите 3 варианта сметы в разных ценовых сегментах</h2>
+       <button class="blue-btn">Рассчитать стоимость</button>
+   </div>
+ </header>
+@endsection
