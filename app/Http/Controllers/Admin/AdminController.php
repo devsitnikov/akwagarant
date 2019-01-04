fd29ff510722f00f9e4b2a707a3864f9c76e1042
Sitnikov Roman <dev.sitnikov@gmail.com>
2019-01-05 00:02:57 +0300

    Start make admin

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 04.01.19
+ * Time: 22:58
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+class AdminController
+{
+    public function index() {
+        return view('admin.index');
+    }
+}
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="card mb-4">
+    <div class="card-body d-sm-flex justify-content-between">
+        <h4 class="pt-1 mb-2">Главная</h4>
+    </div>
+</div>
+@endsection
